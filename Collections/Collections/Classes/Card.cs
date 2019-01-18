76aa3bf6d6481cbe11e6d816177a4b5d021afe18
LutherMckeiver <leoluther96@gmail.com>
2019-01-17 18:43:07 -0800

    changes

--- Collections/Collections/Classes/Card.cs
+++ Collections/Collections/Classes/Card.cs
@@ -6,5 +6,40 @@ namespace Collections.Classes
 {
     class Card
     {
+
+
+        public Suite CardSuite { get; set; }
+        public CardValue Value { get; set; }
+
+
+        public Card(Suite suit, CardValue value)
+        {
+            CardSuite = suit;
+            Value = value;
+        }
+
+    }
+
+
+    enum Suite
+    {
+        Hearts,
+        Diamonds,
+        Spades,
+        Clubs
+    }
+
+    enum CardValue
+    {
+        One,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten
     }
 }
--- Collections/Collections/Classes/Deck.cs
+++ Collections/Collections/Classes/Deck.cs
@@ -4,7 +4,28 @@ using System.Text;
 
 namespace Collections.Classes
 {
-    class Deck
+    public class Deck<T>
     {
+        T[] cards = new T[5];
+        int count;
+
+
+        public void Add(T card)
+        {
+            if (count == cards.Length)
+                Array.Resize(ref cards, cards.Length * 2);
+
+            cards[count++] = card;  
+        }
+
+        public int Count()
+        {
+            return count;
+        }
+
+        public void Remove(T card)
+        {
+
+        }
     }
 }
